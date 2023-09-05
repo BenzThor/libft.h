1115c8977216345cb853e677f0da5c43bb9be187
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-05 19:28:16 +0200

    save

--- 1/Created_Functions/ft_isalnum.c
+++ 1/Created_Functions/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:45:38 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/05 18:43:29 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/05 18:59:23 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 #include "libft.h"
 #include <stdio.h>
 
-
-
 int	ft_isalnum(int c)
 {
 	if ((ft_isalpha(c) == 1) || (ft_isdigit(c) == 1))
 		return (1);
 	return (0);
 }
-
+/*
 int	main(void)
 {
 	if (ft_isalnum(57) == 1)
@@ -30,3 +28,4 @@ int	main(void)
 	else
 		printf("Something went wrong");
 }
+*/
