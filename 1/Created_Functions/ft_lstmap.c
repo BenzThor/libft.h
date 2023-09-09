5e6de4595633f1af89685021e5b525a651383774
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-09 17:20:29 +0200

    save

--- 1/Created_Functions/ft_lstmap.c
+++ 1/Created_Functions/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 16:16:00 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/09 16:29:31 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/09 17:20:14 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	int		len;
 	t_list	*map;
+	t_list	*lst_elem;
 
 	if (!lst || !f || !del)
 		return ;
-	len = ft_lstsize(lst);
-	map = (size_t *)malloc(len * sizeof(size_t));
-	map = lst;
-	ft_lstiter(map, f);
+	map = NULL;
+	while (lst)
+	{
+		lst_elem = ft_lstnew(((*f)(lst->content)));
+		if (!lst_elem)
+			return (NULL);
+		ft_lstadd_back(&lst_elem, map);
+		lst = lst->next;
+	}
 	return (map);
 }
