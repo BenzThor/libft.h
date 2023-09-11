58b43e957e669f6e2d58427fbe592b161ce5e749
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-11 19:16:01 +0200

    save

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:33:12 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/11 18:29:27 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/11 19:15:43 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	ptr1 = (char *)s1;
 	ptr2 = (char *)s2;
-	if (n == 0)
+	if (n == 0 || !s1 || !s2)
 		return (0);
 	while (n > 1 && *ptr1 == *ptr2 && *ptr1 != '\0' && *ptr2 != '\0')
 	{
