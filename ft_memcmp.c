24b652ac6f4979cdb87c7a95bd73d3c2259732de
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-11 19:27:11 +0200

    save

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:33:12 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/11 19:15:43 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/11 19:27:06 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,29 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char	*ptr1;
-	char	*ptr2;
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
 
-	ptr1 = (char *)s1;
-	ptr2 = (char *)s2;
 	if (n == 0 || !s1 || !s2)
 		return (0);
-	while (n > 1 && *ptr1 == *ptr2 && *ptr1 != '\0' && *ptr2 != '\0')
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	while (n > 1 && *ptr1 == *ptr2)
 	{
 		ptr1++;
 		ptr2++;
 		n--;
 	}
-	return ((unsigned char)*ptr1 -(unsigned char)*ptr2);
+	return (*ptr1 - *ptr2);
 }
 /*
+#include <stdio.h>
 int	main(void)
 {
-	char	*s1 = "Wsd";
-	char	*s2 = "Wae";
+	char	s1[] = {0, 0, 127, 0};
+	char	s2[] = {0, 0, 42, 0};
 
-	printf("%d", ft_memcmp(s1, s2, 3));
+	printf("%d", ft_memcmp(s1, s2, 4));
+	printf("%s\n%s", s1, s2);
 }
  */
