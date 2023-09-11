289ba4be58381cfb1d1a425372dd5002d1cab3f1
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-11 19:05:51 +0200

    save

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:13:44 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/11 18:57:35 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/11 19:05:36 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,22 @@ char	*ft_strdup(const char *s)
 	int		len;
 	char	*ptr;
 
+	if (!s)
+		return (NULL);
 	len = ft_strlen(s);
 	ptr = malloc(len + 1);
 	if (ptr == NULL)
 		return (NULL);
 	ft_memcpy(ptr, s, len);
+	ptr[len + 1] = '\0';
 	return (ptr);
 }
 /*
 #include <stdio.h>
 int main(void)
 {
+	char	str[] = "lorem ipsum dolor sit amet";
+	char	*str_dup;
 	printf("%s", ft_strdup("Passt alles?"));
 }
  */
