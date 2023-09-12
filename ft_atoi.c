abed563758fc78dfa77fbdf2cc16f0e565f3e94f
Thorben Benz <tbenz@c1r1p7.42vienna.com>
2023-09-12 17:20:02 +0200

    save

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 11:40:54 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/11 18:51:30 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/12 17:19:24 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_atoi(const char *nptr)
 	int	sum;
 
 	sign = 1;
-	while (*nptr == 32 || (*nptr > 7 && *nptr < 14) )
+	while (*nptr == 32 || (*nptr > 7 && *nptr < 14)
 		nptr++;
 	if (*nptr == 43 || *nptr == 45)
 	{
