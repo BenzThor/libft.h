ac045582525baff93630a72a7e171578ed4e2281
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-12 10:16:18 +0200

    save

--- ft_substr.c
+++ ft_substr.c
@@ -6,17 +6,20 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:37:57 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/08 12:18:42 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/12 10:16:11 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <bsd/string.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
+	int		strlen;
 
+	strlen = ft_strlen(s);
+	if ((strlen + 1) < len)
+		len = strlen;
 	ptr = (char *)malloc(len + 1);
 	if (ptr == NULL)
 		return (NULL);
@@ -24,7 +27,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (ptr);
 }
 /*
+#include <stdio.h>
 int	main(void)
 {
-	printf("%s", ft_substr("Passt alles?", 6, 5));
-} */
+	printf("%s", ft_substr("Passt alles?", 6, 10));
+}
+ */
