2511a369328276084f48559a00d12071319e7e68
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-12 10:45:25 +0200

    save

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:37:57 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/12 10:35:46 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/12 10:45:21 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (ft_strlen(s) < start)
 		len = 0;
-	if (ft_strlen(s + start) < len)
+	else if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
 	ptr = (char *)malloc(len + 1);
 	if (ptr == NULL)
 		return (NULL);
-	ft_strlcpy(ptr, &s[start], len + 1);
+	if (len > 0)
+		ft_strlcpy(ptr, &s[start], len + 1);
+	else
+		ptr[0] = '\0';
 	return (ptr);
 }
-/*
+
 #include <stdio.h>
 int	main(void)
 {
-	printf("%s", ft_substr("tripoille", 100, 1));
+	printf("%s", ft_substr("hola", 4294967295, 0));
 }
- */
