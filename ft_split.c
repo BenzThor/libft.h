e1dc00db1244ed3863ada80cabec3a0f4a661c29
Thorben Benz <tbenz@c1r1p7.42vienna.com>
2023-09-12 15:59:52 +0200

    save

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 09:51:44 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/12 15:57:02 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/12 15:59:30 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static	size_t	ft_str_count(char const *s, char c)
 static void	ft_free(char **arr, int i)
 {
 	while (i >= 0)
-		free(arr[i--];
+		free(arr[i--]);
 	free (arr);
 }
 
