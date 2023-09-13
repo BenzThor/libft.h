9d660b27dcde563024fb18a45daf19be6979fb5d
Thorben Benz <tbenz@c4r7p7.42vienna.com>
2023-09-13 09:30:00 +0200

    save

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 16:16:00 by tbenz             #+#    #+#             */
-/*   Updated: 2023/09/13 09:25:25 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/09/13 09:29:49 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*map;
 	t_list	*lst_elem;
+	t_list	*temp;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	map = NULL;
 	while (lst)
 	{
-		lst_elem = ft_lstnew(((*f)(lst->content)));
+		temp = (*f)(lst->content);
+		lst_elem = ft_lstnew(temp);
 		if (!lst_elem)
 		{
+			(del)(temp);
 			ft_lstclear(&map, del);
 			return (NULL);
 		}
